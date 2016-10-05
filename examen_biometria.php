3fad47e2117f091cb125fc45d02e0f1905151ecd
cr7abraham <madrigal.abraham1994@gmail.com>
2016-10-04 22:26:54 -0500

    CAMBIOS

--- examen_biometria.php
+++ examen_biometria.php
@@ -1,25 +1,7 @@
 <!doctype html>
 <html lang="en-US">
 <head>
-  <style>
-  #section1{
-  margin-left: 30px;
-  margin-top: 60px;
-  max-width: 800px;
-}
-#section2{
-margin-left: 800px;
-margin-top: -772px;
-max-width: 500px;
-}
-#section3{
-margin-left: 30px;
-margin-top: 360px;
-max-width: 800px;
-}
 
-}
-  </style>
   <meta charset="utf-8">
   <meta http-equiv="Content-Type" content="text/html">
   <title>ANÁLISIS GENERAL DE ORINA</title>
@@ -37,53 +19,52 @@ max-width: 800px;
     </span>
 
   <h1>BIOMETRIA HEMATICA</h1>
-  <section id="section1" ALIGN=center>
     <h3>FORMULA ROJA</h3>
-  <form onsubmit="return false">
+  <form onsubmit="return false" ALIGN=center>
   <div >
-    <label style="width: 40em;">
+    <label style="width: 20em;">
       ERITROCITOS MILL/MM3
       <input style="background-color:powderblue">
 
   </div>
   <div >
-    <label  style="width: 40em;">
+    <label  style="width: 20em;">
       HEMOGLOBINA EN GR. %
       <input style="background-color:powderblue; ">
         </label>
   </div>
   <div >
-    <label  style="width: 40em;">
+    <label  style="width: 20em;">
       HEMATOCRITO EN %
       <input style="background-color:powderblue; ">
         </label>
   </div>
   <div >
-    <label   style="width: 40em;">
+    <label   style="width: 20em;">
       H.G.M. UG.
       <input style="background-color:powderblue; ">
         </label>
   </div>
     <div >
-  <label   style="width: 40em;">
+  <label   style="width: 20em;">
     V.G.M. MICRA3
     <input style="background-color:powderblue; ">
       </label>
 </div>
 <div >
-<label   style="width: 40em;">
+<label   style="width: 20em;">
 C.H.G.M %
 <input style="background-color:powderblue; ">
   </label>
 </div>
 <div >
-<label   style="width: 40em;">
+<label   style="width: 20em;">
 PLAQUETAS
 <input style="background-color:powderblue; ">
   </label>
 </div>
 <div >
-<label   style="width: 40em;">
+<label   style="width: 20em;">
 ANOMALÍAS ERITOCÍTICAS
 <input style="background-color:powderblue; ">
   </label>
@@ -91,125 +72,199 @@ ANOMALÍAS ERITOCÍTICAS
 
 
   </form>
-</section>
 
 
-<section id="section2" >
-  <h3>VALORES NORMALES</h3>
-<form onsubmit="return false">
 
-  <div >
-
-    <label   style="width: 25em;">
-
-  ERITROCITOS MILL/MM3:  H 4.6-6.2; M 4.2-5.4
-
-        </label>
-      </div>
-    <div >
-
-    <label   style="width: 25em;">
-
-  HEMOGLOBINA EN GR. %: H 14-18; M 12-16
-
-        </label>
-      </div>
-      <div >
-
-      <label   style="width: 25em;">
-
-      HEMATOCRITO EN %: H 45 - 57; M 40 - 49
-
-          </label>
-        </div>
-        <div >
-
-        <label   style="width: 25em;">
-          H.G.M. UG: 27 - 32
-            </label>
-          </div>
-          <div >
-          <label   style="width: 25em;">
-            V.G.M. MICRA3: 83 - 96
-              </label>
-            </div>
-            <div >
-
-            <label   style="width: 25em;">
-            V.G.M. MICRA3: 83 - 96
-                </label>
-              </div>
-              <div >
-
-              <label   style="width: 25em;">
-                C.H.G.M %: 30 - 35%
-              </label>
-                </div>
-                <div >
-
-                <label   style="width: 25em;">
-                PLAQUETAS: 150,000 - 400,000/mm3
-                    </label>
-                  </div>
-
-
-  </form>
-</section>
-<section id="section3" ALIGN=center>
   <h3>FORMULA BLANCA</h3>
 <form onsubmit="return false">
-<div >
-  <label style="width: 40em;">
-    LEUCOCITOS POR MM3
-    <input style="background-color:powderblue;">
-      </label>
-</div>
-<div >
-  <label  style="width: 40em;">
-    NEOTRÓFILOS TOTALES
-    <input style="background-color:powderblue; ">
-      </label>
-</div>
-<div >
-  <label  style="width: 40em;">
-    HEMATOCRITO EN %
-    <input style="background-color:powderblue; ">
-      </label>
-</div>
-<div >
-  <label   style="width: 40em;">
-    H.G.M. UG.
-    <input style="background-color:powderblue; ">
-      </label>
-</div>
   <div >
-<label   style="width: 40em;">
-  V.G.M. MICRA3
-  <input style="background-color:powderblue; ">
-    </label>
-</div>
-<div >
-<label   style="width: 40em;">
-C.H.G.M %
-<input style="background-color:powderblue; ">
-</label>
-</div>
-<div >
-<label   style="width: 40em;">
-PLAQUETAS
-<input style="background-color:powderblue; ">
-</label>
-</div>
-<div >
-<label   style="width: 40em;">
-ANOMALÍAS ERITOCÍTICAS
-<input style="background-color:powderblue; ">
-</label>
-</div>
+    <label style="width: 25em;" >
+      LEUCOCITOS POR MM3
 
+  </div>
+  <div >
+    <label style="width: 25em;" >
+    <input style="background-color:powderblue; ">
+
+  </div>
+  <div >
+  <label style="width: 25em; " >
+</div>
+<div>
+  <label  style="width: 25em;">
+    NEOTRÓFILOS TOTALES
+
+      </label>
+</div>
+<div >
+<label style="width: 25em;"  >
+<input style="background-color:powderblue; ">
+</div>
+<div >
+<label style="width: 5em;"  >
+%
+</div>
+<div >
+<label style="width: 25em;"  >
+<input style="background-color:powderblue; ">
+</div>
+<div>
+  <label  style="width: 25em;">
+    SEGMENTADOS
+
+      </label>
+</div>
+<div >
+<label style="width: 25em;"  >
+<input style="background-color:powderblue; ">
+</div>
+<div >
+<label style="width: 5em;"  >
+%
+</div>
+<div >
+<label style="width: 25em;"  >
+<input style="background-color:powderblue; ">
+</div>
+<div>
+  <label  style="width: 25em;">
+    EN BANDA
+
+      </label>
+</div>
+<div >
+<label style="width: 25em;"  >
+<input style="background-color:powderblue; ">
+</div>
+<div >
+<label style="width: 5em;"  >
+%
+</div>
+<div >
+<label style="width: 25em;"  >
+<input style="background-color:powderblue; ">
+</div>
+<div>
+  <label  style="width: 25em;">
+    JUVENILES
+
+      </label>
+</div>
+<div >
+<label style="width: 25em;"  >
+<input style="background-color:powderblue; ">
+</div>
+<div >
+<label style="width: 5em;"  >
+%
+</div>
+<div >
+<label style="width: 25em;"  >
+<input style="background-color:powderblue; ">
+</div>
+<div>
+  <label  style="width: 25em;">
+    MIELECITOS
+
+      </label>
+</div>
+<div >
+<label style="width: 25em;"  >
+<input style="background-color:powderblue; ">
+</div>
+<div >
+<label style="width: 5em;"  >
+%
+</div>
+<div >
+<label style="width: 25em;"  >
+<input style="background-color:powderblue; ">
+</div>
+<div>
+  <label  style="width: 25em;">
+    LINFOCITOS
+
+      </label>
+</div>
+<div >
+<label style="width: 25em;"  >
+<input style="background-color:powderblue; ">
+</div>
+<div >
+<label style="width: 5em;"  >
+%
+</div>
+<div >
+<label style="width: 25em;"  >
+<input style="background-color:powderblue; ">
+</div>
+<div>
+  <label  style="width: 25em;">
+    MONOCITOS
+
+      </label>
+</div>
+<div >
+<label style="width: 25em;"  >
+<input style="background-color:powderblue; ">
+</div>
+<div >
+<label style="width: 5em;"  >
+%
+</div>
+<div >
+<label style="width: 25em;"  >
+<input style="background-color:powderblue; ">
+</div>
+<div>
+  <label  style="width: 25em;">
+    EOSINÓFILOS
+
+      </label>
+</div>
+<div >
+<label style="width: 25em;"  >
+<input style="background-color:powderblue; ">
+</div>
+<div >
+<label style="width: 5em;"  >
+%
+</div>
+<div >
+<label style="width: 25em;"  >
+<input style="background-color:powderblue; ">
+</div>
+<div>
+  <label  style="width: 25em;">
+    BASÓFILOS
+
+      </label>
+</div>
+<div >
+<label style="width: 25em;"  >
+<input style="background-color:powderblue; ">
+</div>
+<div >
+<label style="width: 5em;"  >
+%
+</div>
+<div >
+<label style="width: 25em;"  >
+<input style="background-color:powderblue; ">
+</div>
+<div>
+  <label  style="width: 25em;">
+    ANOMALIAS LEUCOCITARIAS
+
+      </label>
+</div>
+<div >
+<label style="width: 40em;"  >
+<input style="background-color:powderblue; ">
+</div>
 
 </form>
-</section>
+
   </div>
 <script type="text/javascript">
 var elems = Array.prototype.slice.call(document.querySelectorAll('.js-switch'));
---
+++ examen_inmunologia.php
@@ -0,0 +1,78 @@
+<!doctype html>
+<html lang="en-US">
+<head>
+  <style>
+  #section1{
+  margin-left: 100px;
+  margin-top: 80px;
+  max-width: 850px;
+}
+
+
+}
+  </style>
+  <meta charset="utf-8">
+  <meta http-equiv="Content-Type" content="text/html">
+  <title>ANÁLISIS GENERAL DE ORINA</title>
+
+  <link rel="stylesheet" type="text/css" media="all" href="css/styles.css">
+  <link rel="stylesheet" type="text/css" media="all" href="css/switchery.min.css">
+  <script type="text/javascript" src="js/switchery.min.js"></script>
+</head>
+<body>
+  <div id="wrapper">
+    <span style="align: left;">
+<a href="menu.html">
+  <img src="img/logo2.png"  style="width: 15%; margin-left: 40px; ">
+</a>
+  </span>
+
+  <h1>INMUNOLOGÍA</h1>
+
+  <form onsubmit="return false" ALIGN=center>
+    <div >
+      <label style="width: 20em;" >
+      R. FEBRILES:
+        <input style="background-color:powderblue">
+    </div>
+    <div >
+      <label style="width: 20em;" >
+      R. DE WIDAL:
+        <input style="background-color:powderblue">
+    </div>
+    <div >
+      <label style="width: 20em;" >
+      TÍFICO O
+        <input style="background-color:powderblue">
+    </div>
+    <div >
+      <label style="width: 20em;" >
+      TÍFICO H
+        <input style="background-color:powderblue">
+    </div>
+    <div >
+      <label style="width: 20em;" >
+      PARATÍFICO A
+        <input style="background-color:powderblue">
+    </div>
+    <div >
+      <label style="width: 20em;" >
+      PARATÍFICO B
+        <input style="background-color:powderblue">
+    </div>
+
+  </form>
+
+
+
+
+  </div>
+<script type="text/javascript">
+var elems = Array.prototype.slice.call(document.querySelectorAll('.js-switch'));
+
+elems.forEach(function(html) {
+  var switchery = new Switchery(html);
+});
+</script>
+</body>
+</html>
