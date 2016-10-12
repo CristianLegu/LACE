1d0db273727de5f0ab40fbdfcc386997793b6ca2
betoG7 <humbertogarciag7@gmail.com>
2016-10-12 16:56:40 -0500

    Alert.php
    Alert php arreglado

--- includes/alert.php
+++ includes/alert.php
@@ -1,11 +1,9 @@
 <?php
 	include("includes/conexion.php");
 
-	if(isset($_SESSION['guarda'])){
-		 $sesion = $_SESSION['guarda'];
+	if(isset($_SESSION['valueF'])){
 	}
 ?>
-
 <html>
 	<header>
 		<script src="js/sweetalert.min.js"></script>
@@ -15,27 +13,21 @@
 	<script type="text/javascript">
 
 			swal({
-  title: "Guardado",
-   text: "Guardado con éxito.",
-    type: "success"
-  }, function(){
-		<?php if(  $sesion == 'PROVEEDOR') { ?>
-		window.location.href = 'proveedores.php';
-
-<?php	} if (  $sesion == 'EMPLEADO' ) { ?>
-window.location.href = 'empleados.php';
-
-<?php	} if (  $sesion == 'MEDICOS' ) { ?>
-
-window.location.href = 'menu_medicos.php';
-
-	<?php		}  ?>
-	<?php	} if (  $sesion == 'PACIENTES' ) { ?>
-
-	window.location.href = 'menu_pacientes.php';
-
-		<?php		}  ?>
-  });
+  				title: "Guardado",
+   				text: "Guardado con éxito.",
+    			type: "success"
+  			}, function(){
+				<?php 	  if(  $sesion == 'PROVEEDOR') { ?>
+					window.location.href = 'proveedores.php';
+				<?php	} if (  $sesion == 'EMPLEADO' ) { ?>
+					window.location.href = 'empleados.php';
+				<?php	} if (  $sesion == 'MEDICOS' ) { ?>
+					window.location.href = 'menu_medicos.php';
+				<?php	} if (  $sesion == 'PACIENTES' ) { ?>
+					window.location.href = 'menu_pacientes.php';
+				<?php   } ?>
+				}
+			);
 	</script>
 
 </html>
