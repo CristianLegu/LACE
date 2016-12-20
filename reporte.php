a34d96eebb000eacff5d7910b220c256e9abe52c
betoG7 <humbertogarciag7@gmail.com>
2016-12-20 10:45:11 -0600

    Reporte
    Plantilla de reporte para LACE

--- reporte.php
+++ reporte.php
@@ -11,11 +11,11 @@ class PDF extends FPDF
             //Logo
             $this->Image('img/conf.png', 10, 8, 33);
             //Fuente
-            $this->SetFont('Arial', 'B', 15);
+            $this->SetFont('Arial', 'B', 14);
             //Espacio
-            $this->Cell(80);
+            $this->Cell(155);
             //Titulo
-            $this->Cell(30, 10, 'Ejemplo', 1, 0 ,'C');
+            $this->Cell(30, 10, 'LACE', 0, 0 ,'C');
             //Salto de linea
             $this->Ln(30);
         }
@@ -28,24 +28,25 @@ class PDF extends FPDF
             //Arial Italic 8
             $this->SetFont('Arial', 'I', 8);
             //Numero de pagina
-            $this->Cell(0, 10, 'Page '.$this->PageNo().'/{nb}', 0, 0, 'C');
+            $this->Cell(0, 10, 'Pagina '.$this->PageNo().'/{nb}', 0, 0, 'C');
 
         }
 
-
+/*
         function AcceptPAgeBreak()
         {
              $this->AddPage();
              $this->SetFillColor(232, 232, 232);
              $this->SetFont('Arial', 'B', 12);
              $this->SetX(10);
-             $this->Cell(70, 6, 'Nombre', 1, 0, 'C', 1);
+             $this->Cell(70, 6, 'Prueba', 1, 0, 'C', 1);
              $this->SetX(80);
-             $this->Cell(20, 6, 'ID', 1, 0, 'C', 1);
+             $this->Cell(20, 6, 'Resultado', 1, 0, 'C', 1);
              $this->SetX(100);
-             $this->Cell(70, 6, 'Usuario', 1, 0, 'C', 1);
+             $this->Cell(70, 6, 'Unidades', 1, 0, 'C', 1);
              $this->Ln();
         }
+*/
     }
     
 
@@ -64,19 +65,30 @@ class PDF extends FPDF
     $pdf->AddPage();
     $pdf->AliasNbPages();
     $pdf->SetFillColor(232, 232, 232);
-    $pdf->SetFont('Arial', 'B', 12);
-
-    $pdf->SetX(20);
-    $pdf->Cell(70, 6, 'Nombre', 1, 0, 'C', 1);
-    
-    $pdf->SetX(90);
-    $pdf->Cell(20, 6, 'ID', 1, 0, 'C', 1);
-    
-    $pdf->SetX(110);
-    $pdf->Cell(70, 6, 'Usuario', 1, 0, 'C', 1);
+    $pdf->SetFont('Arial', 'B', 10);
     
     $pdf->Ln();
+    $pdf->Ln();
 
+    $pdf->SetX(15);
+    $pdf->Cell(30, 6, 'Prueba', 1, 0, 'C', 1);
+    
+    $pdf->SetX(45);
+    $pdf->Cell(60, 6, 'Resultado', 1, 0, 'C', 1);
+    
+    $pdf->SetX(105);
+    $pdf->Cell(25, 6, 'Unidades', 1, 0, 'C', 1);
+    
+    $pdf->SetX(130);
+    $pdf->Cell(25, 6, 'Vl. Referencia', 1, 0, 'C', 1);
+
+    $pdf->SetX(155);
+    $pdf->Cell(40, 6, 'Comentario', 1, 0, 'C', 1);
+
+    
+
+    $pdf->Ln();
+/*
     while($row = mysqli_fetch_array($query, MYSQLI_ASSOC))
     {
         $pdf->SetFont('Arial', '', 12);
@@ -92,7 +104,7 @@ class PDF extends FPDF
         $pdf->Cell(70, 6, $row['n_user'], 1, 1, 'C');
         
     }
-    
+*/    
     $pdf->Output();
     
 
