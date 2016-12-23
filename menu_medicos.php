5b2bc4d7d9bbe2f4016d14258951c4148625e719
Mariano <ianmvc4@gmail.com>
2016-12-23 12:49:23 -0600

    Tablas con Ordenamiento
    Ya estan listas, responsivas y bonitas xD

--- menu_medicos.php
+++ menu_medicos.php
@@ -8,47 +8,48 @@
   <meta charset="utf-8">
   <meta http-equiv="Content-Type" content="text/html">
   <title>Men&uacute; M&eacute;dicos | LACE </title>
-  <link rel="shortcut icon" href="img/icon.png"> 
+  <link rel="shortcut icon" href="img/icon.png">
+  <link rel="stylesheet" type="text/css" media="all" href="css/estilo.css">
   <link rel="stylesheet" type="text/css" media="all" href="css/styles-menu.css">
   <link rel="stylesheet" type="text/css" media="all" href="css/switchery.min.css">
   <link rel="stylesheet" type="text/css" media="all" href="css/bootstrap-switch.css">
   <link rel="stylesheet" type="text/css" media="all" href="css/bootstrap-switch.min.css">
-  <link rel="stylesheet" type="text/css" media="all" href="css/estilo.css">
   <script type="text/javascript" src="js/switchery.min.js"></script>
+  <script type="text/javascript" src="js/script.js"></script>
 </head>
 
 <body>
 
 <nav id="hola">
-  <ul>     
+  <ul>
     <li><p>
           <a href="menu.php">
             <img src="img/logo2.png"  id="logo">
           </a>
         </p>
-          
+
     </li>
-    
+
     <li>
-      <h1>M&eacute;dicos</h1>  
+      <h1>M&eacute;dicos</h1>
     </li>
       <p>
         <form name="formulario" action="" onSubmit="enviarDatos(); return false" autocomplete="off">
           <li><input type="text" placeholder="Buscar..." name="busca" id="busca"></li>
-        </form>  
-      </p>  
+        </form>
+      </p>
     <li>
       <a href="medicos.php" class="add"><img src="img/addmed.png"></a>
     </li>
   </ul>
 </nav>
-  		
 
-      <table id="customers">
+
+      <table class="sortable" id="sorter" >
         <tr>
           <th>Folio</th>
           <th>Nombre</th>
-          <th>Perfil</th>
+          <th class="nosort">Perfil</th>
         </tr>
 
 <?php
@@ -61,11 +62,11 @@
     }
 /*Verifica si el campo busca esta vacio*/
     if(empty($_GET['busca'])){
-        
-          
 
-          
-      $sql = "SELECT 
+
+
+
+      $sql = "SELECT
               count(idmedicos)
               FROM medicos";
       $result = mysqli_query($con, $sql);
@@ -84,24 +85,24 @@
       if(isset($_GET['pn'])){
       	  $pagenum = preg_replace('#[^0-9]#', '', $_GET['pn']);
       }
-    
-      if ($pagenum < 1) { 
-        $pagenum = 1; 
-      } else if ($pagenum > $last) { 
-        $pagenum = $last; 
+
+      if ($pagenum < 1) {
+        $pagenum = 1;
+      } else if ($pagenum > $last) {
+        $pagenum = $last;
       }
-      
+
       $limit = 'LIMIT ' .($pagenum - 1) * $page_rows .',' .$page_rows;
-      
-      $sql = "SELECT  idmedicos, 
-                      nombre 
-              FROM medicos  
-              ORDER BY idmedicos 
+
+      $sql = "SELECT  idmedicos,
+                      nombre
+              FROM medicos
+              ORDER BY idmedicos
               ASC $limit";
       $query = mysqli_query($con, $sql);
 
 
-      
+
       if($last != 1){
           if($pagenum > 1){
             $previous = $pagenum - 1;
@@ -113,7 +114,7 @@
                 }
 	          }
           }
-	        
+
           $paginationCtrls .= ''.$pagenum.' &nbsp; ';
 
           for($i = $pagenum+1; $i <= $last; $i++){
@@ -130,17 +131,17 @@
       }
     }
     else{
-          $pac = $_GET['busca'];  
+          $pac = $_GET['busca'];
           $search = '%'.$pac.'%';
-          $sql = "SELECT 
-                      idmedicos, 
-                      nombre 
+          $sql = "SELECT
+                      idmedicos,
+                      nombre
                     FROM medicos
                   WHERE nombre LIKE '$search'" ;
           $query = $con -> query($sql);
         }
 
-         
+
 
          while ($fila = mysqli_fetch_array($query, MYSQLI_ASSOC)){
          $nombre = $fila['nombre'];
@@ -151,11 +152,11 @@
           <td><a href= "medicos.php?m=<?php echo $fila['idmedicos'] ?>">Ver</a> </td>
         </tr>
 
-<?php } 
+<?php }
   mysqli_close($con);
 ?>
       </table>
-  
+
 <div id="pagination_controls">
       <?php echo $paginationCtrls; ?>
     </div>
@@ -167,5 +168,11 @@ elems.forEach(function(html) {
   var switchery = new Switchery(html);
 });
 </script>
+
+<script type="text/javascript">
+var sorter=new table.sorter("sorter");
+sorter.init("sorter",1);
+</script>
+
 </body>
 </html>
