cb0945e46a4883d36d4233c6092659fe5753daed
betoG7 <humbertogarciag7@gmail.com>
2016-12-24 09:41:45 -0600

    Reporte
    Listo! Al parecer.

--- reporte.php
+++ reporte.php
@@ -1,40 +1,83 @@
 <?php
     include('fpdf181/fpdf.php');
     include("includes/conexion.php");
-    
-    /*
-     if(!isset($_SESSION)) 
-    { 
-        session_start(); 
-    }
-    $idpropio  = $_SESSION['idpropio'];
-    */
-    
-    $idpr   = 0;
-    $idpac  = 0;
-    $idmed  = 0;
-    if(isset($_GET['idpr']) && isset($_GET['idpac']) && isset($_GET['idm'])){
-        $idpr   = $_GET['idpr'];
-        $idpac  = $_GET['idpac'];
-        $idmed  = $_GET['idm'];
-        //echo $idpr;
-    }
-    
+      
+
 class PDF extends FPDF
     {
         //******Cabecera
         function Header()
         {
+
+/**************************************************/
+            include("includes/conexion.php");
+            $idpr   = 0;
+            $idpac  = 0;
+            $idmed  = 0;
+            $queryhead = "";
+            $nombrePaciente = "";
+            $nombreMedico   = "";
+            $fecha = "";
+            if(isset($_GET['idpr']) && isset($_GET['idpac']) && isset($_GET['idm'])){
+                $idpr   = $_GET['idpr'];
+                $idpac  = $_GET['idpac'];
+                $idmed  = $_GET['idm'];
+                
+            }
+            $conhead = mysqli_connect($host, $user, $pwd, $db);
+            $sqlhead = "SELECT  m.nombre AS medico, p.nombre AS paciente, a.fecha AS fecha
+                    FROM analisis AS a 
+                    JOIN pacientes AS p 
+                    ON a.pacientes_idpacientes = p.idpacientes
+                    JOIN medicos m
+                    ON a.medicos_idmedicos = m.idmedicos
+                    WHERE a.idpropio = '$idpr'
+                    ORDER BY a.idpropio";
+
+            $queryhead = $conhead -> query($sqlhead);
+            
+            while($row = mysqli_fetch_array($queryhead, MYSQLI_ASSOC))
+            {
+                $nombreMedico   = $row['medico'];
+                $nombrePaciente = $row['paciente'];
+                $fecha          = $row['fecha'];
+            }
+            
+/***********************************************************/
+
             //Logo
             $this->Image('img/logo2.png', 10, 8, 33);
             //Fuente
             $this->SetFont('Arial', 'B', 14);
             //Espacio
-            $this->Cell(155);
-            //Titulo
-            $this->Cell(30, 10, 'LACE', 0, 0 ,'C');
+            $this->Cell(85);
+            $this->Cell(30, 10, utf8_decode('Laboratorio Espinosa Análisis Clínico'), 0, 0 ,'C');
             //Salto de linea
-            $this->Ln(30);
+            $this->Ln(5);
+            $this->SetFont('Arial', '', 8);
+            $this->Cell(85);
+            $this->Cell(30, 10, utf8_decode('Hidalgo No. 9 Int. 1-A Tel. 469 692 2195 Pénjamo, Gto.'), 0, 0 ,'C');
+            
+            $this->Ln(3.5);
+            $this->SetFont('Arial', '', 8);
+            $this->Cell(85);
+            $this->Cell(30, 10, utf8_decode('Universidad de Gto. Cédula Prof. 1888051 SSA 1931'), 0, 0 ,'C');
+            
+            $this->Ln(14);
+            $this->SetFont('Arial', '', 10);
+            $this->Cell(20);
+            $this->Cell(30, 10, utf8_decode('Examen practicado a: '.$nombrePaciente), 0, 0 ,'C');
+              
+            
+            $this->SetFont('Arial', '', 10);
+            $this->Cell(105);
+            $this->Cell(30, 10, utf8_decode('Fecha: '.$fecha), 0, 0 ,'C');
+
+            $this->Ln(5);
+            $this->SetFont('Arial', '', 10);
+            $this->Cell(20);
+            $this->Cell(30, 10, utf8_decode('Solicitado por el (la) Dr. (a): '.$nombreMedico), 0, 0 ,'C');
+            
         }
 
 //******Pie de pagina
@@ -65,13 +108,22 @@ class PDF extends FPDF
         }
 */
     }
+      
+    $idpr   = 0;
+    $idpac  = 0;
+    $idmed  = 0;
+    $queryhead = "";
+    if(isset($_GET['idpr']) && isset($_GET['idpac']) && isset($_GET['idm'])){
+        $idpr   = $_GET['idpr'];
+        $idpac  = $_GET['idpac'];
+        $idmed  = $_GET['idm'];
+        
+    }
     
-
-
 
     $con = mysqli_connect($host, $user, $pwd, $db);
 
-    $sql = "SELECT a.prueba, a.resultado, a.unidades, a.valorreferencia, a.comentario, a.observaciones
+    $sql = "SELECT  a.prueba, a.resultado, a.unidades, a.valorreferencia, a.comentario, a.observaciones
             FROM analisis AS a 
             JOIN pacientes AS p 
             ON a.pacientes_idpacientes = p.idpacientes
@@ -82,6 +134,7 @@ class PDF extends FPDF
 
     
     $query = $con -> query($sql);
+
     
     
     $pdf = new PDF();
@@ -93,22 +146,22 @@ class PDF extends FPDF
     $pdf->SetTextColor(255, 255, 255);
 
     $pdf->Ln();
-    $pdf->Ln();
+    $pdf->Ln(5);
 
     $pdf->SetX(15);
     $pdf->Cell(30, 6, 'Prueba', 1, 0, 'C', 1);
     
     $pdf->SetX(45);
-    $pdf->Cell(60, 6, 'Resultado', 1, 0, 'C', 1);
+    $pdf->Cell(100, 6, 'Resultado', 1, 0, 'C', 1);
     
-    $pdf->SetX(105);
+    $pdf->SetX(145);
     $pdf->Cell(25, 6, 'Unidades', 1, 0, 'C', 1);
     
-    $pdf->SetX(130);
+    $pdf->SetX(170);
     $pdf->Cell(25, 6, 'Vl. Referencia', 1, 0, 'C', 1);
 
-    $pdf->SetX(155);
-    $pdf->Cell(40, 6, 'Comentarios', 1, 0, 'C', 1);
+   // $pdf->SetX(155);
+    //$pdf->Cell(40, 6, 'Comentarios', 1, 0, 'C', 1);
 
     
     $pdf->Ln();
@@ -123,16 +176,16 @@ class PDF extends FPDF
         $pdf->Cell(30, 6, $row['prueba'], 1, 0, 'C');
         
         $pdf->SetX(45);
-        $pdf->Cell(60, 6, $row['resultado'], 1, 0, 'C');
+        $pdf->Cell(100, 6, $row['resultado'], 1, 0, 'C');
         
-        $pdf->SetX(105);
+        $pdf->SetX(145);
         $pdf->Cell(25, 6, $row['unidades'], 1, 0, 'C');
         
-        $pdf->SetX(130);
-        $pdf->Cell(25, 6, $row['valorreferencia'], 1, 0, 'C');
+        $pdf->SetX(170);
+        $pdf->Cell(25, 6, $row['valorreferencia'], 1, 1, 'C');
         
-        $pdf->SetX(155);
-        $pdf->MultiCell(40, 6, $row['observaciones'], 1, 'C', false);
+        //$pdf->SetX(155);
+        //$pdf->MultiCell(40, 6, $row['observaciones'], 1, 'C', false);
 
         $observaciones = $row['comentario'];
         
@@ -148,7 +201,18 @@ class PDF extends FPDF
         $pdf->SetX(20);
         $pdf->MultiCell(170, 6, $observaciones, 1, 'J', true);
         
-  
+        $pdf->Ln(50);
+        $pdf->SetX(20);
+        $pdf->SetFillColor(255, 255, 255);
+        $pdf->SetTextColor(0, 0, 0);
+        $pdf->Cell(180, 6, 'Atentamente', 0, 1, 'C');
+
+        $pdf->Ln(20);
+        $pdf->SetX(20);
+        $pdf->SetFillColor(255, 255, 255);
+        $pdf->SetTextColor(0, 0, 0);
+        $pdf->Cell(180, 6, 'QFB. Fabiola Espinosa Bribiesca', 0, 1, 'C');
+
         $pdf->Output();
 
 ?>
