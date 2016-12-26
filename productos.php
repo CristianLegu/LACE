b58f7af2bf6426d2dc013507665eefc4024dcd5a
CristianLegu <cristian.glg94@gmail.com>
2016-12-26 13:56:36 -0600

    estilo productos y proveedores
    encabezado

--- productos.php
+++ productos.php
@@ -15,7 +15,7 @@
   <meta http-equiv="Content-Type" content="text/html">
   <title>Productos | LACE</title>
   <link rel="shortcut icon" href="img/icon.png">
-  <link rel="stylesheet" type="text/css" media="all" href="css/styles2.css">
+  <link rel="stylesheet" type="text/css" media="all" href="css/styles.css">
   <link rel="stylesheet" type="text/css" media="all" href="css/switchery.min.css">
   <script type="text/javascript" src="js/switchery.min.js"></script>
   <!-- Pantalla de carga-->
@@ -39,14 +39,22 @@
           </div>
         </div>
   <!-- Pantalla de carga-->
-  <div id="wrapper">
+<nav id="hola">
+  <ul>
+    <li><p>
+          <a href="menu_productos.php">
+	        <img src="img/logo2.png" id="logo">
+        </a>
+        </p>
 
-    <span style="align: left;">
-      <a href="menu_productos.php">
-       <img src="img/logo2.png"  style="width: 15%; margin-left: 40px; ">
-     </a>
-     <h1>Productos</h1>
-   </span>
+    </li>
+
+    <li>
+      <h1>Productos</h1>
+    </li>
+  </ul>
+</nav>
+
 
    <form action="guarda.php" action="guarda.php" method="post" autocomplete="off">
 
