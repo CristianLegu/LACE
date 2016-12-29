f55cb66bdbeecae1896327f8720fe01098c26574
CristianLegu <cristian.glg94@gmail.com>
2016-12-29 13:15:51 -0600

    fechas reporte
    ya esta, solo probaré las cosas en lo que lo recibe

--- menu_reporte.php
+++ menu_reporte.php
@@ -6,9 +6,14 @@
 		<title>Menú Reporte</title>
 		<link rel="shortcut icon" href="img/icon.png">
 		<link rel="stylesheet" type="text/css" href="css/default.css" />
-		<link rel="stylesheet" type="text/css" href="css/menu.css" />
 		<link rel="stylesheet" type="text/css" href="css/component.css" />
 		<script src="js/modernizr.custom.js"></script>
+
+		<link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+  		<script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+ 		<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+
+
 	</head>
   <body>
     <div class="container">
@@ -49,8 +54,8 @@
                   </li>
 
                   <li>
-                    <a target="_blank" href="reporte_gral_analisis.php">
-						<span class="icon">
+                    <a href="#" class="icon">
+						<span >
 						    <i aria-hidden="true" class="icon-pdf"></i>
 						</span>
 				        <span>Mostrar análisis</span>
@@ -66,26 +71,47 @@
 							      </a>
                   </li>
                   <li style="visibility:hidden;">
-                    <a href="">
-								    <span class="icon">
-									    <i aria-hidden="true" class="icon-services"></i>
-								    </span>
-								    <span>Visualizar PDF</span>
-							      </a>
+                    <a >
+						<span >
+						   <i aria-hidden="true" class="icon-services"></i>
+					    </span>
+					    <span>Visualizar PDF</span>
+					</a>
                   </li>
               </ul>
           </nav><!--NAV-->
       </div> <!--class = "main clearfix"-->
     </div> <!--Cointainer-->
+
+	<div id="dynamic_field" >
+		<form name="fechas" method="get" action="reporte_gral_analisis.php" target="_blank" ALIGN=center autocomplete="off">
+			<div>
+				<label style="font-size: 15px;">De: <input type="text" id="datepicker" name="f1" required></label>
+				<label style="font-size: 15px;">A:  <input type="text" id="datepicker2" name="f2" required ></label>
+				<button class="guardar" >Enviar</button>
+			</div>
+		</form>
+	</div>
   </body>
 
 </html>
 
+<script type="text/javascript">
 
- <script type="text/javascript">
-     // var prop  = <?php echo $idpropio; ?>;
-     // var pac   = <?php echo $idpaciente; ?>;
-     // var med   = <?php echo $medicos_idmedicos; ?>;
-     // alert(prop+" "+pac+" "+med);
-     // window.open("reporte.php?idpr=" + prop + "&idpac=" + pac + "&idm=" + med, "_blank");
+ $(document).ready(function(){
+document.getElementById('dynamic_field').style.display = 'none';	
+	 $(document).on('click', '.icon', function(){
+		 $( function() {
+				$( "#datepicker" ).datepicker();
+			} );
+			 $( function() {
+				$( "#datepicker2" ).datepicker();
+			} );
+		document.getElementById('dynamic_field').style.display = 'block';	
+      });
+
+  });
+
+
  </script>
+
